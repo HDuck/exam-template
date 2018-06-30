68036e75468b663310fef20326d6e8ab4ba05adf
HDuck <HardcoreDuck@users.noreply.github.com>
2018-06-30 19:10:03 +0300

    Build 30.06 v8

--- docs/js/video-btn.js
+++ docs/js/video-btn.js
@@ -4,17 +4,6 @@ var videoDesc = document.getElementById("video-desc");
 
 playBtn.addEventListener('click', function() {
   player.play();
-}, false)
-
-player.addEventListener('click', function() {
-  if (videoDesc.className === "video-pres__desc video-pres__desc_hidden")
-    player.pause();
-}, false)
-
-player.addEventListener('play', function() {
   videoDesc.className = "video-pres__desc video-pres__desc_hidden";
-}, false)
-
-player.addEventListener('pause', function() {
-  videoDesc.className = "video-pres__desc video-pres__desc_visible";
+  player.controls = "true";
 }, false)
